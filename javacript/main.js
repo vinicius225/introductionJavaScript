65b4b40f1605092f39402cba1804a9360d82efda
vinicius225 <vinicius.guilherme225@gmail.com>
2021-11-04 12:46:43 -0300

    Agora vai

--- javacript/main.js
+++ javacript/main.js
@@ -12,11 +12,45 @@ lista.reverse();// lista ao contrario
 lista.toString();// traz em forma de string
 lista.join("-"); //substitui a virgula pelo que quiser
 
-
 var frutas ={cor:"azul",sabor:"ruim"};
 frutas.cor;
 
 var data = new Date(); // Pegar a data
-console.log(data.getDate())
+console.log(data.getDate())//pegar um campo expecifico da data
 
 console.log(data);
+
+
+function soma(n1,n2){
+    return n1+n2
+}
+
+
+function ValidatorYears(idade){
+    if(idade<18){
+        return"Pode entrar não";
+    }else {
+        return "Pode entrar"
+    }
+}
+
+function Botao(){
+    console.log(document.getElementById("agradecimento")); // Pegar algo no html por ID
+    console.log(document.getElementById("agradecimento").innerHTML= "<h3>Ae siim</h3>"); // Adicionar codigos
+}
+
+
+function Redirect(){
+   // window.open("https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Meta_programming"); // Abrir uma pagina WEB
+    window.location.href= "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Meta_programming";//Abre na mesma paginas
+    
+}
+
+//this no html passa o elemento na função
+// onchange é usado no select utiliza para chamar função no html
+function Torcar(item){
+    var x = document.getElementById("esse").innerHTML("Valeu mermão")
+    item.value
+
+
+}
