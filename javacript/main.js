c1531c9eadeeb0977db2645ed3ec628415c0234c
vinicius andrade <vinicius225@hotmail.com>
2021-11-04 11:15:00 -0300

    Inicio

---
+++ javacript/main.js
@@ -0,0 +1,22 @@
+
+var nome = "Vinicius Guilherme de Freitas";
+var idade = 29
+
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("Uva");//adicionar um elemnento
+console.log(lista);
+lista.pop();//remover o ultimo
+//lista.length(); //tamanho da lista;
+lista.reverse();// lista ao contrario
+lista.toString();// traz em forma de string
+lista.join("-"); //substitui a virgula pelo que quiser
+
+
+var frutas ={cor:"azul",sabor:"ruim"};
+frutas.cor;
+
+var data = new Date(); // Pegar a data
+console.log(data.getDate())
+
+console.log(data);
